2229979ba7a92aab1ca6e3a88c6216f4871f2b36
Singgi Mokodompit <singgimokodompit_it@gmail.com>
2019-12-05 00:21:56 +0700

    add file * by Singgi Mokodompit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Penjualan</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<!-- container -->
+		<div class="container">
+			
+			<!-- nav -->
+				<div class="nav">
+					<ul>
+						<li><a href="">Home</a></li>
+					</ul>
+				</div>
+			<!-- end nav -->
+			<!-- header -->
+				<div class="header">
+					<h2>Aplikasi Penjualan</h2>
+				</div>
+			<!-- end header -->
+			
+			<!-- table -->
+				<div class="table">
+					
+
+						<table border="1" cellpadding="5" cellspacing="0">
+
+							<div class="add">
+								<form action="">
+									<button style="width: 200px;">Tambah Barang</button>
+								</form>
+							</div>
+					<h2>Daftar Barang</h2>
+							<tr>
+								<th>NO</th>
+								<th>KODE BARANG</th>
+								<th>NAMA BARANG</th>
+								<th>HARGA</th>
+								<th>PERSEDIAAN</th>
+								<th>OPSI</th>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>12345</td>
+								<td>Keyboard</td>
+								<td>100000</td>
+								<td>2</td>
+								<td>
+									<form action="">
+										<button>Edit</button>
+									</form>
+									<form action="">
+										<button>Delete</button>
+									</form>
+								</td>
+							</tr>
+
+						</table>
+
+				</div>
+			<!-- end table -->
+		</div>
+	<!-- end container -->
+	
+</body>
+</html>
